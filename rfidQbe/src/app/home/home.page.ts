4b8d7d9545120a8e9adca0a188afb2f01513bfc7
javaSingh <javasingh11@gmail.com>
2019-03-07 17:50:34 +0530

    fillFormFrmArray

--- rfidQbe/src/app/home/home.page.ts
+++ rfidQbe/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,13 @@ import { HttpClient } from '@angular/common/http';
 export class HomePage implements OnInit {
 
   formGroup: FormGroup
+  results: any[] = []
+  index
 
   constructor(
     public fb: FormBuilder,
     public http: HttpClient,
+    public toastController: ToastController
   ) {
     console.log('Constructor')
   }
@@ -23,7 +27,8 @@ export class HomePage implements OnInit {
     this.formGroup = this.fb.group({
 
       'assetType': [''],
-      'yearOfManufacture': [''],
+      'yearOfManufacture': ['',
+        [Validators.pattern('[0-9]{1,2}'), Validators.min(0), Validators.max(99)]],
       'owner': [''],
       'vehicleType': [''],
       'serialNo': [''
@@ -60,17 +65,89 @@ export class HomePage implements OnInit {
 
   onSubmit() {
     console.log(this.formGroup.value, this.formGroup.valid)
-    var jsonData=this.formGroup.value
+    var jsonData = this.formGroup.value
     console.log(JSON.stringify(this.formGroup.value))
     Object.keys(jsonData).forEach(k => (!jsonData[k] && jsonData[k] !== undefined) && delete jsonData[k]);
     console.log(JSON.stringify(this.formGroup.value))
-    this.http.get('http://172.16.22.64:3000/Tags/v1/EPC/qbe?filter=' + JSON.stringify(this.formGroup.value), { reportProgress: true }).subscribe(data => {
+    /*     if (jsonData['yearOfManufacture']) {
+          console.log(jsonData['yearOfManufacture'].substring(2, 4))
+          jsonData['yearOfManufacture'] = jsonData['yearOfManufacture'].substring(2, 4)
+        } */
+    console.log(jsonData)
+
+    this.http.get('http://172.16.22.64:3000/Tags/v1/EPC/qbe?filter=' + JSON.stringify(this.formGroup.value), { reportProgress: true }).subscribe((data: any[]) => {
       console.log('HTTP GET Result: ', data)
+      this.results = data
+      /*           this.formGroup.patchValue({
+                  assetType:[this.results[0].asset_type.value]
+                }) */
+      if (this.results.length > 0)
+        this.showResults(0)
+      else {
+        this.presentToast('No Result Found')
+      }
     }, error => {
       console.log('HTTP GET ERROR: ', error)
     })
 
   }
+
+  showResults(index: any) {
+    console.log('Showing Reults. Index is: ', index)
+    this.index = index
+    console.log(this.results[0].year_of_manufacture)
+    this.formGroup.controls['assetType'].setValue('' + this.results[index].asset_type)
+    this.formGroup.controls['yearOfManufacture'].setValue("" + this.results[0].year_of_manufacture)
+    this.formGroup.controls['owner'].setValue('' + this.results[index].owner)
+    this.formGroup.controls['vehicleType'].setValue('' + this.results[index].vehicle_type)
+    this.formGroup.controls['serialNo'].setValue('' + this.results[index].serial_no)
+    this.formGroup.controls['dateUse'].setValue('' + this.results[index].date_use)
+    this.formGroup.controls['vehicleCode'].setValue('' + this.results[index].vehicle_mfc_code)
+
+    /* asset_type: "F"
+date_use: "2029-07-17"
+gs1_code: 8907709
+owner: "ECOR"
+serial_no: "003927"
+vehicle_mfc_code: "ARC"
+vehicle_type: "BOXNHL"
+year_of_manufacture: 17 */
+
+  }
+
+  showPrevious() {
+    console.log('Showing Previous')
+    this.showResults(this.index - 1)
+  }
+
+  showNext() {
+    console.log("Showing Next")
+    this.showResults(this.index + 1)
+  }
+
+  async presentToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      translucent: false,
+      showCloseButton: true,
+      closeButtonText: 'CLOSE',
+      duration: 1500,
+      position: "top"
+    });
+    toast.present();
+  }
+
+  clearSelection(key){
+    console.log('Inside clearSelection('+key+')')
+    if(this.formGroup.controls[key].value){
+      console.log('Select value Found for',key)
+    }
+  }
+
+  customAlertOptions: any = {
+    header: 'Owner',
+    translucent: true,
+  };
 
 
 }
