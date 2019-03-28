f791effe0ce8545e1c82a43f7494512be4d8ab89
javaSingh <javasingh11@gmail.com>
2019-03-28 11:07:29 +0530

    htmlNoCmt&GridHeadWip

--- rfidQbe/src/app/constants/constants.ts
+++ rfidQbe/src/app/constants/constants.ts
@@ -1,4 +1,4 @@
-export const CONTEXT_ROOT= 'api/';
+export const CONTEXT_ROOT = 'api/';
 
 //for online deployment
 //export const BASE_URL= 'https://hero.indianrail.gov.in/'; 
@@ -7,6 +7,8 @@ export const CONTEXT_ROOT= 'api/';
 // export const BASE_URL= '';  
 
 //for offline deployment
+// http://10.60.201.247:3002/  -> https://roams.cris.org.in/rfidApi/
+// export const BASE_URL = 'https://roams.cris.org.in/rfidApi'
 export const BASE_URL='http://10.60.201.247:3002';
 // export const BASE_URL='http://172.16.22.64:3000';
 
